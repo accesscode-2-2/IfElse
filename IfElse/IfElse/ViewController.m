d8829fc8643a2b83f89427de9bb47a84a26ed250
Christella <cdolm92@gmail.com>
2015-06-06 17:52:01 -0400

    First Commit: checking for asterisks

--- IfElse/IfElse/ViewController.m
+++ IfElse/IfElse/ViewController.m
@@ -10,18 +10,37 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField1;
+@property (weak, nonatomic) IBOutlet UITextField *textField2;
+@property (weak, nonatomic) IBOutlet UITextField *textField3;
+
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+- (IBAction)buttonTapped:(id)sender {
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+ 
+    
+    
+    NSString *one = self.textField1.text;
+    NSString *two = self.textField2.text;
+    NSString *three = self.textField3.text;
+  
+    // Resource used: http://stackoverflow.com/questions/2753956/how-do-i-check-if-a-string-contains-another-string-in-objective-c
+    
+    
+    if ([one rangeOfString:@"*"].location == NSNotFound) {
+        NSLog(@"%@", one);
+        NSLog(@"Approved");
+    } else {
+        NSLog(@"%@", one);
+        NSLog(@"Please make sure that your statement does not contain the following disallowed character: *");
+    }
 }
+    
+
 
 @end
