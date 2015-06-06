3269d3e40092310cf1e2342af0bed71855107d6a
ayunav <ayunanyc@gmail.com>
2015-06-06 17:22:29 -0400

    commit_Ayuna

--- IfElse/IfElse/ViewController.m
+++ IfElse/IfElse/ViewController.m
@@ -10,18 +10,45 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *field1;
+
+@property (weak, nonatomic) IBOutlet UITextField *field2;
+
+@property (weak, nonatomic) IBOutlet UITextField *field3;
+
+@property (weak, nonatomic) IBOutlet UISwitch *toggle;
+
 @end
+
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)buttonTapped:(id)sender {
+    NSLog(@"signed in");
+    
+    NSString *name = self.field1.text;
+    NSString *em = @"nyc@gmail.com";
+    NSLog(@"%@", name);
+
+    NSString *pass1 = self.field2.text;
+    NSLog(@"%@", pass1);
+
+    NSString *pass2 = self.field3.text;
+    NSLog(@"%@", pass2);
+    
+    BOOL isTheEmail = [name isEqualToString:em];
+    
+    BOOL oneEqualTwo = [pass1 isEqualToString:pass2];
+    if (oneEqualTwo && isTheEmail) {
+        NSLog(@"correct password");
+        [self.toggle setOn:YES animated:YES];
+    } else {
+        NSLog(@"incorrect name or password");
+        [self.toggle setOn:NO animated:YES];
+    }
+    
 }
 
 @end
