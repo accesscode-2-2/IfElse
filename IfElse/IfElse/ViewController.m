4f6fbd69981f4abbad1854e38e5ba060c657604b
Mesfin <mesbekmek@gmail.com>
2015-06-06 16:33:45 -0400

    Added implementation of button and text fields

--- IfElse/IfElse/ViewController.m
+++ IfElse/IfElse/ViewController.m
@@ -10,18 +10,45 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField1;
+@property (weak, nonatomic) IBOutlet UITextField *textField2;
+@property (weak, nonatomic) IBOutlet UITextField *textField3;
+@property (weak, nonatomic) IBOutlet UISwitch *toggle;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)buttonTapped:(id)sender {
+    NSLog(@"tapped");
+    
+    NSString *one = self.textField1.text;
+    NSString *two = self.textField2.text;
+    NSString *three = self.textField3.text;
+
+    BOOL  oneEqualsTwo = [one isEqualToString:two];
+    
+    if(oneEqualsTwo){
+      //  NSLog(@"First two are equal");
+        [self.toggle setOn:YES animated:YES];
+    }else{
+        [self.toggle setOn:NO animated:YES];
+    }
+    
+    
+    NSInteger intFromString1 = [one integerValue];
+    NSInteger intFromString2 = [two integerValue];
+    
+    if(intFromString1==intFromString2){
+          NSLog(@"You extracted integers from the string bruh!");
+        [self.toggle setOn:YES animated:YES];
+    }else{
+        [self.toggle setOn:NO animated:YES];
+    }
+        
+    
+    NSLog(@"%@",one);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
