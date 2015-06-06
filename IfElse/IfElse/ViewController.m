9ca59297a339d4dea9f2c584cebd2dffb56b697e
Jason Wang <qwang216@gmail.com>
2015-06-06 17:43:51 -0400

    Better Version

--- IfElse/IfElse/ViewController.m
+++ IfElse/IfElse/ViewController.m
@@ -9,19 +9,45 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField1;
+@property (weak, nonatomic) IBOutlet UITextField *textField2;
+@property (weak, nonatomic) IBOutlet UITextField *textField3;
+@property (weak, nonatomic) IBOutlet UISwitch *toggle;
+@property (weak, nonatomic) IBOutlet UITextView *resultTextView;
+
+
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-@end
+- (IBAction)buttonTapped:(id)sender {
+  /*
+    
+    NSLog(@"Tapped");
+    NSString *textfield1Value = self.textfield1.text;
+    NSLog(@"%@", textfield1Value);
+  */
+    
+        NSString *one = self.textField1.text;
+        NSString *two = self.textField2.text;
+        NSString *three = self.textField3.text;
+        NSString *override = @"override";
+    
+        BOOL oneEqualsTwo = [one isEqualToString:two];
+        BOOL isThree = [three isEqualToString:override];
+    
+        if (oneEqualsTwo) {
+            [self.toggle setOn:YES animated:YES];
+            self.resultTextView.text = @"Congraz! it Matches!";
+        } else if (isThree) {
+            [self.toggle setOn:YES animated:YES];
+            self.resultTextView.text = @"Nice! it been Override!";
+        } else {
+            [self.toggle setOn:NO animated:YES];
+            self.resultTextView.text = @"ERRO";
+        }
+    
+    
+    
+    }
+    @end
