abcba42683dc65da221e71bd0d240a41f7d03d84
Jackie Meggesto <jackie.meggesto@gmail.com>
2015-06-06 17:11:30 -0400

    Weird little slider

--- IfElse/IfElse/ViewController.m
+++ IfElse/IfElse/ViewController.m
@@ -10,18 +10,42 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField1;
+@property (weak, nonatomic) IBOutlet UITextField *textField2;
+@property (weak, nonatomic) IBOutlet UITextField *textField3;
+
+@property (weak, nonatomic) IBOutlet UIProgressView *progressBar;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)buttonTapped:(id)sender {
+    NSLog(@"tapped");
+    
+    NSString *one = self.textField1.text;
+    NSString *two = self.textField1.text;
+    NSString *three = self.textField1.text;
+    
+    if ([one isEqualToString:(@"33")]) {
+        [self.progressBar setProgress:(.50)];
+        NSLog(@"You've cracked the code!");
+    } else if ([one isEqualToString:(@"22")]) {
+        NSLog(@"You've made some progress...!");
+        [self.progressBar setProgress:(.33)];
+    } else if ([one isEqualToString:(@"66")]){
+        [self.progressBar setProgress:(.66)];
+        NSLog(@"Making more progress now!");
+    } else {
+        [self.progressBar setProgress:(.99)];
+        NSLog(@"Good job! Full progress!");
+    }
+    
+    
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
