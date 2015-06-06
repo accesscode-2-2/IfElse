5b09d5f6f5af47c7482d987fbe18cb18a60026c6
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-06 18:02:24 -0400

    Did this actually do the right thing? In class

--- IfElse/IfElse/ViewController.m
+++ IfElse/IfElse/ViewController.m
@@ -10,18 +10,67 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textFieldOne;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldTwo;
+
+@property (weak, nonatomic) IBOutlet UISwitch *toggle;
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldThree;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldFour;
+@property (weak, nonatomic) IBOutlet UISwitch *bottomToggle;
+
 @end
+
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+
+- (IBAction)topButt:(id)sender {
+    NSLog(@"tapped");
+    
+    NSString *one = self.textFieldOne.text;
+    NSString *two = self.textFieldTwo.text;
+//    NSString *three = self.textFieldThree.text;
+//    NSString *four = self.textFieldThree.text;
+    
+    BOOL oneEqualsTwo = [one isEqualToString:two];
+    if (oneEqualsTwo) {
+        [self.toggle setOn:YES animated:YES];
+    } else {
+        [self.toggle setOn:NO animated:YES];
+    }
+    
+    NSString *textFieldOneValue = self.textFieldOne.text;
+    NSLog(@"%@", textFieldOneValue);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//  THE ONE ABOVE IS ALREADY DONE FOR YOU. MAKE THE SECOND PART
+
+- (IBAction)bottomButt:(id)sender {
+    NSLog(@"tipped");
+    
+    NSString *three = self.textFieldThree.text;
+    NSString *four = self.textFieldThree.text;
+    
+    BOOL threeEqualsFour = [three isEqualToString:four];
+    if (threeEqualsFour) {
+        [self.bottomToggle setOn:YES animated:YES];
+    } else {
+        [self.bottomToggle setOn:NO animated:YES];
+    }
+    
+    
+    
+    NSString *textFieldThreeValue = self.textFieldThree.text;
+    NSLog(@"%@", textFieldThreeValue);
+    
+    
+    
+    
+    
+    
+    
 }
 
 @end
