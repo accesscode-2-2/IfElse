3c90249dbf055d9e1d933e3af41e2b19d0a0a8ea
Henna <henna.ahmed92@gmail.com>
2015-06-06 17:42:50 -0400

    Made a BASIC sign-up form

--- IfElse/IfElse/ViewController.m
+++ IfElse/IfElse/ViewController.m
@@ -9,19 +9,48 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *cond1;
+@property (weak, nonatomic) IBOutlet UITextField *cond2;
+@property (weak, nonatomic) IBOutlet UITextField *cond3;
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)onSubmit:(id)sender {
+    NSString *name = self.cond1.text,
+            *email = self.cond2.text,
+            *emailVerify = self.cond3.text;
+    
+    
+    BOOL nameExists = [name length];
+    BOOL emailsAreSame = [email isEqualToString:emailVerify];
+    
+    BOOL isEmail = [email rangeOfString:@"^.+@.+\\..{2,}$" options:NSRegularExpressionSearch].location != NSNotFound;
+    BOOL isEmailV = [emailVerify rangeOfString:@"^.+@.+\\..{2,}$" options:NSRegularExpressionSearch].location != NSNotFound;
+    
+    if(nameExists){
+    if (isEmail && isEmailV) {
+    
+    if(emailsAreSame){
+        self.outputLabel.text = [NSString stringWithFormat:@"Welcome %@!", name];
+    }
+    else {
+        self.outputLabel.text = [NSString stringWithFormat:@"Emails not the same"];
+    }
+    }
+    else {
+        self.outputLabel.text = [NSString stringWithFormat:@"Please enter valid e-mail addresses"];
+    }
+    }
+    else {
+         self.outputLabel.text = [NSString stringWithFormat:@"Please enter your name"];
+    }
+    
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
